adfaceb4c5eee061bcecb4d28ea83aafb136c452
asterisk1o9 <asterisk1o9@gmail.com>
2022-06-16 15:59:18 -0400

    up

--- TwoSum/TwoSum.cpp
+++ TwoSum/TwoSum.cpp
@@ -26,33 +26,6 @@ TwoSum::TwoSum(vector<int> v, int t){
  
 
 //BRUTE FORCE
-/*
-void TwoSum::Solution(){
-    
-    for(i = start + 1; i < values.size(); i++)//i is always greater then our starting position
-    {
-        sum = values[start] + values[i];//add the first and second position
-        if(sum == target){
-            cout<< "[" << start << "," << i << "]" << "\n";//if we found the numbers that add to target then print
-            break;
-        }
-        
-        else if(start == values.size() - 2 && i == values.size() - 1 && sum != target)
-        {
-            cout << "Target not found\n";//if start is equal to the second to last position and i is equal to the last position and the target is not found the print not found
-            break;
-        }
-         
-    }
-        if(sum != target && i == values.size())
-        {
-            start++;//if we've gone through every position at i up to the size of the vector move our starting position up 1
-            Solution();//recall our function to start at the new positions
-        }
-}
-*/
-
-//BRUTE FORCE
 vector<int> TwoSum::twoSum(vector<int>& nums, int target){
     for(i = start + 1; i < nums.size(); i++)//i is always greater then our starting position
     {
