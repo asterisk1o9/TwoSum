270178d7d7eb4b6bad363f2fdd45642032adb516
asterisk1o9 <asterisk1o9@gmail.com>
2022-06-16 16:06:52 -0400

    update

--- TwoSum/TwoSum.hpp
+++ TwoSum/TwoSum.hpp
@@ -21,14 +21,8 @@ using namespace std;
 
 class TwoSum{
  public:
-    //vector<int> values;//variable accessible to the cpp file but not required because already defined in cpp
-    //int target;//variable accessible to the cpp file but not required because already defined in cpp
-    //TwoSum(std::vector<int> v, int t);
     
     vector<int> twoSum(vector<int>& nums, int target);
-    
-    //void find_Two();//For method with prints
-    //void loop();//For method with prints
 };
 
  
